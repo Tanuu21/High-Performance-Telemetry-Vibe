8f898b03c02b94bb123541331b47da58654548af
Tanmayyy2122 <165697277+Tanuu21@users.noreply.github.com>
2026-06-01 09:26:38 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./*.{js,ts,jsx,tsx,mdx}"
+    "./pages.js",
+    "./*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -20,4 +17,5 @@ module.exports = {
   },
   plugins: [],
 }
+
 
